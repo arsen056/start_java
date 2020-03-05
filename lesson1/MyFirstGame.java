1645acbe290aa556bda5f774c57ab0ea86816635
Arsen Vurdihanov <vurdihanov@ya.ru>
2020-03-05 21:43:04 +0300

    Rename class

---
+++ lesson1/MyFirstGame.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+class MyFirsGame {
+	public static void main(String[] args) {
+		int a =(int) (Math.random()*100);
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean bool = true;
+		System.out.println("загаданное число " + a);
+		System.out.println("Введите число");
+
+		 while(bool) {
+			int number_user = scanner.nextInt();
+
+			if (number_user>a){
+				System.out.println("Повторите попытку, ваше число больше загаданного числа");
+			}
+
+			else if (number_user<a) {
+				System.out.println("Повторите попытку, ваше число меньше загаданного числа");
+			}
+
+			else if (number_user==a) {
+				System.out.println("Вы угадали число");
+				bool = false;
+			}
+
+		}
+
+	}
+
+}
