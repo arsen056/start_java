5870bd7b2f3d3ffe217b3e65f1f1abe264d9448a
Arsen Vurdihanov <vurdihanov@ya.ru>
2020-03-05 21:28:40 +0300

    moving files to a folder

---
+++ lesson1/Variable.java
@@ -0,0 +1,31 @@
+class Variable {
+	public static void main(String[] args) {
+
+			byte core = 4;
+			short price = 20000;
+			int size = 15;
+			long system_capacity = 64;
+			float cpu_frequency = 2.30f;
+			double weight = 1.5;
+			char symbol = 'A';
+			boolean t = true;
+			
+			System.out.println("core:" + core);
+
+			System.out.println("price:" + price);
+
+			System.out.println("size:" + size);
+
+			System.out.println("Razryadnost:" + system_capacity);
+
+			System.out.println("chastota:" + cpu_frequency);
+
+			System.out.println("weight:" + weight);
+
+			System.out.println("symbol:" + symbol);
+
+			System.out.println("boolean:" + t);
+
+		}	
+
+}
