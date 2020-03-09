eab1ce6e55a6b4ea3ba454c3140374fec72820d0
Arsen Vurdihanov <vurdihanov@ya.ru>
2020-03-09 20:56:22 +0300

    Add lesson2/person

---
+++ lesson2/Person.java
@@ -0,0 +1,26 @@
+class Person {
+	String gender = "male";
+	String name = "Arsen";
+	double height = 1.80;
+	int age = 22;
+
+	void go (){
+		System.out.println("Человек идет");
+	}
+
+	void sit (){
+		System.out.println("Человек сидит");
+	}
+
+	void run (){
+		System.out.println("Человек бежит");
+	}
+
+	void speak (){
+		System.out.println("Человек говорит");
+	}
+
+	void learnJava (){
+		System.out.println("Человек учит Java");
+	}
+}
