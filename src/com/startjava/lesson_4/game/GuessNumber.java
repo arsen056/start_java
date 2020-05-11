19e33e71e1ae7209e2aae670cd61a9a401ec674e
arsen056 <vurdihanov@gmail.com>
2020-05-11 14:03:28 +0400

    lesson 4, error correction

--- src/com/startjava/lesson_4/game/GuessNumber.java
+++ src/com/startjava/lesson_4/game/GuessNumber.java
@@ -11,6 +11,28 @@ class GuessNumber {
 		this.pl2 = pl2;
 	}
 
+	public void inputNumbers(Player pl, int number, int randNum, int forAttempt) {
+		if (number > randNum) {
+			System.out.println("Ваше число больше загаданного числа");
+			pl.setAttempt(forAttempt + 1);
+			pl.setEnteredNums(forAttempt, number);
+		} else if (number < randNum) {
+			System.out.println("Ваше число меньше загаданного числа");
+			pl.setAttempt(forAttempt + 1);
+			pl.setEnteredNums(forAttempt, number);
+		} else if (number == randNum) {
+			pl.setAttempt(forAttempt + 1);
+			System.out.println(pl.getName() + " Вы угадали число c " + pl.getAttempt() + " попытки");
+			pl.setEnteredNums(forAttempt, number);
+		}
+	}
+
+	public void outputNumbers(Player pl) {
+		for (int i = 0; i < pl.getEnteredNums().length; i++) {
+			System.out.print(pl.getEnteredNums()[i] + " ");
+		}
+	}
+
 	public void guessNumber() {				
 		int randNum = (int) (Math.random() * 101);
 		System.out.println("игра началась! загаданное число " + randNum);
@@ -20,7 +42,7 @@ class GuessNumber {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(pl1.getName() + " введите число");
 			playerNum1 = scanner.nextInt();
-			pl1.inputNumbers(playerNum1, randNum, i);
+			inputNumbers(pl1, playerNum1, randNum, i);
 			if (playerNum1 == randNum) {
 				break;
 			}
@@ -31,7 +53,7 @@ class GuessNumber {
 
 			System.out.println(pl2.getName() + " введите число");
 			playerNum2 = scanner.nextInt();
-			pl2.inputNumbers(playerNum2, randNum, i);
+			inputNumbers(pl2, playerNum2, randNum, i);
 			if (playerNum2 == randNum) {
 				break;
 			}
@@ -41,10 +63,10 @@ class GuessNumber {
 			}
 		}
 		System.out.print(pl1.getName() + " Ваши числа ");
-		pl1.outputNumbers();
+		outputNumbers(pl1);
 		pl1.fill(0);
 		System.out.print("\n" + pl2.getName() + " Ваши числа ");
-		pl2.outputNumbers();
+		outputNumbers(pl2);
 		pl2.fill(0);
 	}
 }
--- src/com/startjava/lesson_4/game/Player.java
+++ src/com/startjava/lesson_4/game/Player.java
@@ -1,11 +1,11 @@
 package com.startjava.lesson_4.game;
+
 import java.util.Arrays;
 
 class Player {
 	private String name;
-	private int number;
 	private int attempt;
-	private int[] enteredPlayer = new int[10];
+	private int[] enteredNums = new int[10];
 
 	Player(String name) {
 		this.name = name;		
@@ -15,54 +15,23 @@ class Player {
 		return name;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public int getAttempt () {
+	public int getAttempt() {
 		return attempt;
 	}
 
-	public void setAttempt (int attempt) {
+	public void setAttempt(int attempt) {
 		this.attempt = attempt;
 	}
 
-	public int[] getEnteredPlayerCopy() {
-		return Arrays.copyOf(enteredPlayer, attempt);
+	public int[] getEnteredNums() {
+		return Arrays.copyOf(enteredNums, attempt);
 	}
 
-	public void setEnteredPlayer (int index, int number) {
-		enteredPlayer[index] = number;
+	public void setEnteredNums(int index, int number) {
+		enteredNums[index] = number;
 	}
 
-	public void fill (int number) {
-		Arrays.fill(enteredPlayer, number);
-	}
-
-	public void inputNumbers(int number, int randNum, int forAttempt) {
-		setNumber(number);
-		if (getNumber() > randNum) {
-			System.out.println("Ваше число больше загаданного числа");
-			setAttempt(forAttempt + 1);
-			setEnteredPlayer(forAttempt, number);
-		} else if (getNumber() < randNum) {
-			System.out.println("Ваше число меньше загаданного числа");
-			setAttempt(forAttempt + 1);
-			setEnteredPlayer(forAttempt, number);
-		} else if (getNumber() == randNum) {
-			setAttempt(forAttempt + 1);
-			System.out.println(getName() + " Вы угадали число c " + getAttempt() + " попытки");
-			setEnteredPlayer(forAttempt, number);
-		}
-	}
-
-	public void outputNumbers () {
-		for (int i = 0; i < getEnteredPlayerCopy().length; i++) {
-			System.out.print(getEnteredPlayerCopy()[i] + " ");
-		}
+	public void fill(int number) {
+		Arrays.fill(enteredNums, number);
 	}
 }
